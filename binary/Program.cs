26d185472a290313e5cfcb30bd32fbf16a9c0a03
garipoff <asafgar@yandex.ru>
2019-10-11 14:25:17 +0300

    second commit

--- binary/Program.cs
+++ binary/Program.cs
@@ -8,8 +8,10 @@ namespace binary
         {
             int left = array.GetLowerBound(0);//Определяем начальный индекс массива
             int right = array.GetUpperBound(0);//Определяем конечный индекс массива
-            if (left == right)//Если массив состоит из одного значения
-                return left;//Возвращаем 0
+            if (left == right && array[left] == a)//Если массив состоит из одного значения и оно равно числу
+                return left;//Возвращаем 0 
+            if (left == right && array[left] != a)//Если массив состоит из одного значения и оно не равно числу
+                return -1;//Возвращем -1
             while (true)
             {
                 if (right - left == 1)//Если левый и правый элементы соседи
@@ -33,23 +35,56 @@ namespace binary
                 }
             }
         }
-        static void Main(string[] args)
+
+        public static int[] Arr()
         {
-            int[] arr = new int [20];//Создаём массив
+            int[] arr = new int[20];//Создаём массив
             Random rnd = new Random();
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = rnd.Next(0, 255);//Заполняем массив сгенерированными числами
             }
             Array.Sort(arr);//Сортируем массив по возрастанию
+            return arr;
+        }
 
+        public static void Type()
+        {
+            int[] arr = Arr();
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.Write("{0} ",arr[i]);//Выводим отсортированный массив
+                Console.Write("{0} ", arr[i]);//Выводим отсортированный массив
             }
-            Console.Write("\nВведите число для поиска: ");//Запрашиваем число для поиска
-            int a = Convert.ToInt32(Console.ReadLine());//Конвертируем введённое значение в число
-            Console.WriteLine("index: {0}",BinarySearch(arr, a));//Передаём методу BinarySearh отсортированный массив и число для поиска. Выводим результат
+
+            while (true)
+            {
+                Console.Write("\nВведите число для поиска: ");//Запрашиваем число для поиска
+                try
+                {
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int result = BinarySearch(arr, a);//Передаём методу BinarySearh отсортированный массив и число для поиска
+
+                    switch (result)
+                    {
+                        case -1:
+                            Console.WriteLine("{0} не найден", a); //Выводим результат, если число не найдено
+                            break;
+                        default:
+                            Console.WriteLine("{0} найден. Индекс: {1}", a, result);//Выводим результат, если число найдено
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный формат входных данных");//Выходим из цикла в случае ошибки
+                    break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Type();
         }
     }
 }
